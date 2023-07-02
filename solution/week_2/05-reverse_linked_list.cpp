0c4b52a30e0961f75b83e6599f7b979101892f25
Asım Kaymak <kaymakasm@gmail.com>
2023-07-02 15:06:54 +0300

    Create 05-reverse_linked_list.cpp

---
+++ solution/week_2/05-reverse_linked_list.cpp
@@ -0,0 +1,40 @@
+/* Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: [] */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* new_link = new ListNode(0);
+        while(head!=NULL){
+            ListNode* tmp_node = new ListNode(head->val,new_link->next);
+            new_link->next = tmp_node;
+            head = head->next;
+        }
+        return new_link->next;
+    }
+};
